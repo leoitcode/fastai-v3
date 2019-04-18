d36e0c1bba56d0759975f6f4efc5002a240fb750
leoitcode <46280942+leoitcode@users.noreply.github.com>
2019-04-18 15:28:34 -0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=13Nxml5y0VVrn7J8GjTuxZDO1WwR2YslX'
+export_file_url = 'https://drive.google.com/uc?export=download&id=153usHTwuEUpNP5OfN85shCCIn66-Lwoc'
 export_file_name = 'export.pkl'
 
 classes = ['macbook', 'notmacbook']
